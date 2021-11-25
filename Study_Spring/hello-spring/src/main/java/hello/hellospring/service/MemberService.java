e79298cee18f664b917f25613e723ad29368e4cd
haekang <ydjm1027@naver.com>
2021-11-25 13:41:51 +0900

    스프링빈등록_자바코드로 등록

--- Study_Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Study_Spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -9,30 +9,29 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public class MemberService {
 
     // 이러면 약간 memoryMemberRepo랑 MemberService랑 둘이 각각 다른 리포 쓰는 느낌나자너
     //private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     // 외부에서 repo 넣어주도록 설정
-    private final MemoryMemberRepository memoryMemberRepository;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public MemberService(MemoryMemberRepository memoryMemberRepository){
-        this.memoryMemberRepository = memoryMemberRepository;
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
 
     // 회원 가입
     public Long join(Member member){
         validateDuplicateMember(member); // 중복회원 검증
-        memoryMemberRepository.save(member);
+        memberRepository.save(member);
         return member.getId();
     }
 
     private void validateDuplicateMember(Member member){
         // 같은 이름이 있는 중복 회원 X
-        memoryMemberRepository.findByName(member.getName())
+        memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
@@ -40,11 +39,11 @@ public class MemberService {
 
 
     public List<Member> findMembers(){
-        return memoryMemberRepository.findAll();
+        return memberRepository.findAll();
     }
 
     public Optional<Member> findOne(Long memberId){
-        return memoryMemberRepository.findById(memberId);
+        return memberRepository.findById(memberId);
     }
 
 }
